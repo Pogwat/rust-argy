f520ca344ac4c0f336483d584f2e98d7331d6fd3
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-04-23 21:02:54 -0400

    add some more test stuff

--- src/main.rs
+++ src/main.rs
@@ -33,87 +33,104 @@ fn main() {
 
     }
 
-macro_rules! concat_const_arrays {
-    ($type:ty, $($arr:expr),*) => {{
-        const LEN: usize = $($arr.len() + )* 0;
-        const RES: [$type; LEN] = {
-            let mut res = [0; LEN];
-            let mut offset = 0;
-            $(
-                let arr = $arr;
-                let mut i = 0;
-                while i < arr.len() {
-                    res[offset + i] = arr[i];
-                    i += 1;
-                }
-                offset += arr.len();
-            )*
-            res
-        };
-        RES
-    }};
-}
+// macro_rules! concat_const_arrays {
+//     ($type:ty, $($arr:expr),*) => {{
+//         const LEN: usize = $($arr.len() + )* 0;
+//         const RES: [$type; LEN] = {
+//             let mut res = [0; LEN];
+//             let mut offset = 0;
+//             $(
+//                 let arr = $arr;
+//                 let mut i = 0;
+//                 while i < arr.len() {
+//                     res[offset + i] = arr[i];
+//                     i += 1;
+//                 }
+//                 offset += arr.len();
+//             )*
+//             res
+//         };
+//         RES
+//     }};
+// }
 
-
-    macro_rules! argv_to_n_s {
-        
-           ( $( (
-                name: $name:literal,
-                ident: $ident1:literal $(,$ident2plus:literal)*,
-                description: $description:expr
-            ) ),* )
-         => {{   
-                const LEN:usize = 0 $($name; 1)*;
-
-                let mut tmp_table = [ArgEntry<'static>; LEN]
-                let mut tmp_index = 0;
-
-
-                $( 
-
-                
-                static ent: ArgEntry<'static> = {
-                    name: & $name,
-                    description: & $description
-                };
-                
-                tmp_table[tmp_index] = ($name, ent);
-                tmp_index = tmp_index+1;
-
-
-                )*
-         }}
-        }
-
-        let cx = argv_to_n_s!{(name:"a", ident:"b", description:"c" )};
-
-
-    const  x:[u8;2] = [1,2];
-    const  y:[u8;2]= [3,4];
-    let xy = concat_const_arrays!(u8,x,y);
-
-    #[derive(Clone)]
-    struct ArgEntry<'a> {
-        name: &'a str,
-        description: &'a str
+    const fn argy_entry(identity: &'static  str , name:&'static str, description:&'static str) -> (&'static str, ArgEntry) {
+        (identity, ArgEntry { name, description})
     }
 
-    static TEST: ArgEntry<'static> = ArgEntry{
-        name: "by",
-        description: "no"
-    };
+    static MY_ENTRY: (&'static str, ArgEntry) = argy_entry("id", "name", "desc");   
+    println!("{}",MY_ENTRY.1.description);
 
-    static PHF_TABLE: phf::Map<&'static str, [&'static str; 2]> = gen_phf_arg_table! {
-        (name: "help", ident: "helpy", description: "Print help"),
-         (name: "helpa", ident: "helpya", description: "Print help")
-    };
+    static arrayb: [(&'static str, ArgEntry); 2] = [
+        argy_entry("id", "name", "desc"),
+        argy_entry("id2", "name2", "desc2")
+    ];
 
-    static PHF_TABLE2: phf::Map<&'static str, &'static ArgEntry> = gen_phf_arg_table! {
-        (ident: "helpy", data: &TEST)
-    };
+ 
+    macro_rules! make_big_const_array {
+        ( varible_type: $var_type:ident,  element_type: $element_type:ty, entries:  $($entry:expr)*,)=> {
+            $var_type array: [$element_type;  ${count($entry)}] = [$($entry,)*]
+        }
+    }
 
-    let value = &PHF_TABLE["helpy"];
-    value.iter().for_each(|v|println!("{}",v));
+    // macro_rules! argv_to_n_s {
+        
+    //        ( lifetime: $life:tt , $( (
+    //             name: $name:literal,
+    //             ident: $ident1:literal $(,$ident2plus:literal)*,
+    //             description: $description:expr
+    //         ) ),* )
+    //      => {{   
+    //             const LEN: usize = 0 $(+ { $name; 1 })*;
+
+    //             let mut tmp_table = [(ArgEntry<$life>); LEN];
+    //             let mut tmp_index = 0;
+
+
+    //             $( 
+
+                
+    //             static ent: ArgEntry<$life> = {
+    //                 ArgEntry {
+    //                     name: &$name,
+    //                     description: &$description,
+    //                     // ... other fields
+    //                 }
+    //             };
+                
+    //             tmp_table[tmp_index] = ($name, ent);
+    //             tmp_index = tmp_index+1;
+
+
+    //             )*
+    //      }}
+    //     }
+
+        //let cx = argv_to_n_s!(lifetime: 'a , (name:"a", ident:"b", description:"c" ));
+
+
+    // const  x:[u8;2] = [1,2];
+    // const  y:[u8;2]= [3,4];
+    // let xy = concat_const_arrays!(u8,x,y);
+
+    #[derive(Clone)]
+    struct ArgEntry {
+        name: &'static str,
+        description: &'static str
+    }
+
+
+    // static PHF_TABLE: phf::Map<&'static str, [&'static str; 2]> = gen_phf_arg_table! {
+    //     (name: "help", ident: "helpy", description: "Print help"),
+    //      (name: "helpa", ident: "helpya", description: "Print help")
+    // };
+
+    // static PHF_TABLE2: phf::Map<&'static str, &'static ArgEntry> = gen_phf_arg_table! {
+    //     (ident: "helpy", data: &TEST)
+    // };
+
+    // let value = &PHF_TABLE["helpy"];
+    // value.iter().for_each(|v|println!("{}",v));
 
 
 
