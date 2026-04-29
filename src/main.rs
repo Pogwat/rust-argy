48a132ea9778b7b1a5c3ee2b7bca5afba5795815
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-04-29 19:58:25 -0400

    use the ArgEntry struct

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,51 @@ fn main() {
     println!("Hello, world!");
     
     struct ArgEntry {
-        identifier: &'static str,
         name: &'static str,
-        description: &'static str
+        description: &'static str,
+        arg_type: &'static str
     }
 
-    struct Argy<const N:usize> {
-        args: phf::Map<&'static str, (&'static str, &'static str)>,
+    struct Argy<const N:usize
+    //, const F:usize
+    > {
+        args: phf::Map<&'static str, ArgEntry>,
         fargs: [&'static str; N]
+        //func_asserts: [(&'static str, fn (&str) -> bool); F]
     }
+
+    //const defualt_asserts: [(&'static str, fn (&str)); 3]
 
     macro_rules! make_argy_map {
-        (    $(  ( $($identifier:expr),+ => $name:expr, $description:expr  )    ),*    ) => {
-                phf::phf_map! { $(   $($identifier) | +  =>    ($name,  $description)     ),* }
+        (    $(  ( $($identifier:expr),+ => $name:expr, $description:expr, $arg_type:expr  )    ),*    ) => {
+                phf::phf_map! { $(   $($identifier) | +  =>    ArgEntry { name:$name,  description:$description, arg_type:$arg_type }     ),* }
         }
     }
 
-    static ARGS: phf::Map<&'static str, (&'static str, &'static str)> = make_argy_map!(
-        ("a", "d" => "b", "c"), ("g" => "b", "c")
+    static ARGS: phf::Map<&'static str, ArgEntry> = make_argy_map!(
+        ("a", "d" => "b", "c", "e"), ("g" => "b", "c", "e")
     ); 
 
-    type ArgyMap = phf::Map<&'static str, (&'static str, &'static str)>;
+    type ArgyMap = phf::Map<&'static str, ArgEntry>;
     impl <const N: usize>Argy<N> {
 
-        parse_args_from_array<T, N:usize>(&mut self,args_array: [T,N]) {
-            args_array.iter().for_each(|entry| {
-                if map.contains_key(entry) {}
-            }
+        // fn parse_args_from_array<T, N:usize>(&mut self,args_array: [T,N]) {
+        //     args_array.iter().for_each(|entry| {
+        //         if self.args.contains_key(entry) {}
+        //     }
 
 
 
-            )
-        }
+        //     )
+        // }
+
+        // fn assert_flag(&self, string: &str) ->bool {
+        //     if let Some(key) = self.args.key(string) {
+        //         assert_eq!(key.arg_type, "flag")
+        //     }
+        // }
+
+        // fn assert_
 
 
     }
@@ -52,8 +65,8 @@ fn main() {
     }   
 
     let  (x,a)  =make_argies!( 
-        make_argy_map!(("a", "d" => "b", "c"), ("g" => "b", "c") ),
-        make_argy_map!(("a", "d" => "b", "c"), ("g" => "b", "c") ) 
+        make_argy_map!(("a", "d" => "b", "c", "e"), ("g" => "b", "c", "e") ),
+        make_argy_map!(("a", "d" => "b", "c", "e"), ("g" => "b", "c", "e") ) 
     ); 
     
 }
