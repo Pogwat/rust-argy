dd0cf259b7c992d5e46e363b17db08cf4d6092a3
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-04-28 23:10:31 -0400

    add some argy generator macros

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,53 @@ fn main() {
         description: &'static str
     }
 
-    macro_rules! make_argy {
+    struct Argy<const N:usize> {
+        args: phf::Map<&'static str, (&'static str, &'static str)>,
+        fargs: [&'static str; N]
+    }
+
+    macro_rules! make_argy_map {
         (    $(  ( $($identifier:expr),+ => $name:expr, $description:expr  )    ),*    ) => {
                 phf::phf_map! { $(   $($identifier) | +  =>    ($name,  $description)     ),* }
         }
     }
 
-    static ARGS: phf::Map<&'static str, (&'static str, &'static str)> = make_argy!(
-        ("a", "d" => "b", "c")
-    
-    
-    
+    static ARGS: phf::Map<&'static str, (&'static str, &'static str)> = make_argy_map!(
+        ("a", "d" => "b", "c"), ("g" => "b", "c")
+    ); 
+
+    type ArgyMap = phf::Map<&'static str, (&'static str, &'static str)>;
+    impl <const N: usize>Argy<N> {
+
+        parse_args_from_array<T, N:usize>(&mut self,args_array: [T,N]) {
+            args_array.iter().for_each(|entry| {
+                if map.contains_key(entry) {}
+            }
+
+
+
+            )
+        }
+
+
+    }
+
+    macro_rules! make_argies {
+        (  $($argy_map:expr),+  ) => {
+            (
+                $(
+                    Argy {
+                        args:$argy_map,
+                        fargs: [""; $argy_map.len()]
+                    }
+                ),+
+            )
+        }
+    }   
+
+    let  (x,a)  =make_argies!( 
+        make_argy_map!(("a", "d" => "b", "c"), ("g" => "b", "c") ),
+        make_argy_map!(("a", "d" => "b", "c"), ("g" => "b", "c") ) 
     ); 
     
 }
